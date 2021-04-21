3cb8ce22c276da5a9e82803332a9ce75695ad3f6
Ankita Gupta <65870621+ankitagupta820@users.noreply.github.com>
2021-04-20 20:50:03 -0700

    BST

---
+++ Size_Of_Binary_Tree.py
@@ -0,0 +1,34 @@
+"""
+Find the size of Binary tree i.e number of total nodes in the tree
+"""
+
+
+class TreeNode:
+    def __init__(self, val: int, left: TreeNode, right: TreeNode):
+        self.val = val
+        self.left = left
+        self.right = right
+
+            
+# Inductive approach 
+def size(root: TreeNode):
+  
+    if root is None:
+        return 0  
+    return size(root.left) + size(root.right) + 1
+
+    
+    
+# Traverse and accumulate approach
+def size(root: TreeNode):
+    size = 0
+    def getSize(node: TreeNode):
+        # base case
+        if node is None:
+            return 0
+        else:
+            size+= getSize(node.left) + getSize(node.right) + 1
+    
+    getSize(root)
+    return size;
+        
