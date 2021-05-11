d116f6d20378d67830d9df3b41a7c3f919db2b4e
Ankita Gupta <65870621+ankitagupta820@users.noreply.github.com>
2021-05-11 16:29:49 -0700

    Medium_75
    https://leetcode.com/problems/sort-colors/

---
+++ Sort_colors(Dutch Flag).py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        
+        p0, p1, p2 = 0, 0, len(nums)-1
+        while p1<=p2:
+            if nums[p1]==0:
+                nums[p1], nums[p0] = nums[p0], nums[p1]
+                p0+=1
+                p1+=1
+                
+            elif nums[p1]==1:
+                p1+=1
+            else:
+                nums[p1], nums[p2] = nums[p2], nums[p1]
+                p2-=1
+                
+        
