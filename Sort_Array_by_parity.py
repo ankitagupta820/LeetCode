0cddd79eab0dbb95babfe1956c251cd71daadd75
Ankita Gupta <65870621+ankitagupta820@users.noreply.github.com>
2021-05-11 14:06:44 -0700

    Easy_905
    https://leetcode.com/problems/sort-array-by-parity/

--- Sort_Array_by_parity.py
+++ Sort_Array_by_parity.py
@@ -1,4 +1,8 @@
 class Solution:
+    
+    """
+    Used skip-skip-action
+    """
     def sortArrayByParity(self, A: List[int]) -> List[int]:
         
         l,r =0, len(A)-1
@@ -7,8 +11,11 @@ class Solution:
             
             if A[l]%2 == 0:
                 l+=1
+            elif A[r]%2 == 1:
+                r-=1
             else:
                 A[l], A[r] = A[r], A[l]
+                l+=1
                 r-=1
-        
+                
         return A
